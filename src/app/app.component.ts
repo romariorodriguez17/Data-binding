186dcc01d5071d9ad9ea649b9b1dd97887fadc08
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-28 09:33:09 -0500

    se añade el event-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'databing';
+  name = 'Interpolation'
+
+  textoPlaceholder = 'property binding '
+  deshabilitado: boolean = true
+  imgSrc = 'https://blog.knoldus.com/wp-content/uploads/2020/06/angular.png'
+
+  // event-binding
+  texto: string = 'esto es even-binding'
+  constructor() {
+    // interpolation
+    setTimeout(() => {
+      this.name = 'Data Binding (interpolation)'
+    }, 2000)
+
+    //Property binding
+    setTimeout(() => {
+      this.deshabilitado = false
+    }, 3000)
+  }
+  // llamando metodo (interpolation )
+  getSuma(numero1: number, numero2: number) {
+    return numero1 + numero2
+  }
+
+  cambiarTexto(): void {
+    this.texto = 'probando cambio de texto para probar el event-binding'
+  }
+
+
 }
